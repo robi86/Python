f22269ff33f85c987bbc2ac633d1a32a68cff13d
amir-jafari <amir.h.jafari@okstate.edu>
2018-01-22 14:27:50 -0500

    Updated

--- 6- Math_Linear_Algebra/Math_LinearAlgebera_Exercise.py
+++ 6- Math_Linear_Algebra/Math_LinearAlgebera_Exercise.py
@@ -27,58 +27,59 @@ print(logsig)
 
 # ----------------------------------------------------------------------------------------------------------------------
 
-# Q-2: a) Generate 100 samples that has uniform distribution. Plot your results and verify it.
+# Q-2: a) Generate 100 samples points from a uniform distribution. Plot your results and verify it.
 
 
 
 
 # ----------------------------------------------------------------------------------------------------------------------
 
-# Q-2: b)Generate 1000 samples that has normal distribution (mean zero and std 1). Plot your results and verify it.
+# Q-2: b)Generate 1000 samples points from normal distribution (with mean zero and std 1). Plot your results and verify it.
 
 
 
 
 # ----------------------------------------------------------------------------------------------------------------------
-# Q-3: a)Write a list of numbers and convert it to array.
+# Q-3: a)Generate a list of numbers (random or arbitary) and convert it to a numpy array.
 
 
 
 
 # ----------------------------------------------------------------------------------------------------------------------
 
-# Q-3: b)Create 1x3, 3x3 and 5x5 matrix. Use different methods that you know. It can be random or manually enter numbers.
+# Q-3: b)Create 1x3, 3x3 and 5x5 size matrices. Use different python methods to generates these matrices that you know of (It can be random or manually enter numbers).
 
 
 
 
 # ----------------------------------------------------------------------------------------------------------------------
-# Q-3: c) Multiply each one of those matrix by each other element wise and matrix multiply. Check your results by hand.
-#         just the first two matrix.
+# Q-3: c) Multiply the first two matrix by the other elementwise and matrix multiplication if it is possible. Check your results by hand.
 
-
-
-
-# ----------------------------------------------------------------------------------------------------------------------
-# Q-4: a) Create a matrix 3x3. Add the first and second column then sum the the results.
 
 
 
 
 # ----------------------------------------------------------------------------------------------------------------------
-# Q-4: b) Create a vector of 100 samples (any method you want). Plot the vector. Calcuate the mean and std.
+# Q-4: a) Create a matrix size of 3x3. Create a new varibale and name it var then add the first and second column of matrix then sum the the vector. Save the results in var.
 
 
 
 
 # ----------------------------------------------------------------------------------------------------------------------
-# Q-4: c) Create a matrix 3x3 and 3x1. Multiply the first matrix by the second matrix. Define a logsig transfer function.
-#         pass the results of the the multiplication to the transferfunction. Check does it make sense. Explain your results
+# Q-4: b) Create a vector of 100 samples (with any python method that you have in your mind). Plot the vector. Calcuate the mean and std.
+
 
 
 
 # ----------------------------------------------------------------------------------------------------------------------
-# Q-5: a) Calculate the expanded form of (x+y)^6.
+# Q-4: c) Create a matrix 3x3 and 3x1. Multiply the first matrix by the second matrix. 
+#         Define a logsig transfer function.
+#         Pass the results of the the multiplication to the transfer function (vector vize). Check does it make sense. Explain your results
+
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Q-5: a) Expand (x+y)^6.
 
 
 
